519ce9517f708fab3ed4525d0201ce9f57b9c175
ysawiris <youssef.sawiris@students.dominican.edu>
2021-01-07 01:39:56 +0200

    init push

---
+++ server/resturants/forms.py
@@ -0,0 +1,10 @@
+
+from django import forms
+
+from resturants.models import Document
+
+
+class DocumentForm(forms.ModelForm):
+    class Meta:
+        model = Document
+        fields = ('description', 'document', )
